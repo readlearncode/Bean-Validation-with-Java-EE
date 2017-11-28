ae899123f1b8b2c525578f7011cf50fc186b1bb1
Alex Theedom <alex.theedom@gmail.com>
2017-11-28 13:15:00 -0800

    add tests to start and end state

--- 03_05_start/src/test/java/com/readlearncode/model/StockTest.java
+++ 03_05_start/src/test/java/com/readlearncode/model/StockTest.java
@@ -2,12 +2,8 @@ package com.readlearncode.model;
 
 import org.junit.Test;
 
-import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
-import java.util.Set;
-
-import static org.assertj.core.api.Java6Assertions.assertThat;
 
 /**
  * Source code github.com/readlearncode
@@ -21,18 +17,21 @@ public class StockTest {
 
     @Test
     public void givenStockPOJO_whenCodeValid_shouldValidate()   {
-        Stock stock = new Stock();
-        stock.setCode("XXX");
-        Set<ConstraintViolation<Stock>> violations = validator.validateProperty(stock, "code");
-        assertThat(violations.size()).isEqualTo(0);
+
     }
 
     @Test
     public void givenStockPOJO_whenCodeNull_shouldNotValidate()   {
-        Stock stock = new Stock();
-        stock.setCode(null);
-        Set<ConstraintViolation<Stock>> violations = validator.validateProperty(stock, "code");
-        assertThat(violations.size()).isEqualTo(1);
+
     }
 
+    @Test
+    public void givenStockPOJO_whenCodeTooShort_shouldNotValidate()   {
+
+    }
+
+    @Test
+    public void givenStockPOJO_whenCodeTooLong_shouldNotValidate()   {
+
+    }
 }
--- 03_05_start/src/test/java/com/readlearncode/model/TransactionTest.java
+++ 03_05_start/src/test/java/com/readlearncode/model/TransactionTest.java
@@ -2,15 +2,11 @@ package com.readlearncode.model;
 
 import org.junit.Test;
 
-import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Set;
-
-import static org.assertj.core.api.Java6Assertions.assertThat;
 
 /**
  * Source code github.com/readlearncode
@@ -25,28 +21,36 @@ public class TransactionTest {
 
     @Test
     public void givenTransactionPOJO_whenDataNull_shouldNotValidate() throws ParseException {
-        Transaction transaction = new Transaction();
-        transaction.setQuantity(null);
-        transaction.setPriceLimit(null);
-        transaction.setType(null);
-        transaction.setStock(null);
-        transaction.setExerciseDate(null);
 
-        Set<ConstraintViolation<Transaction>> violations = validator.validate(transaction);
-        assertThat(violations.size()).isEqualTo(5);
     }
 
     @Test
     public void givenTransactionPOJO_whenDataValid_shouldValidate() throws ParseException {
-        Transaction transaction = new Transaction();
-        transaction.setQuantity(10);
-        transaction.setPriceLimit(123.00);
-        transaction.setType(Transaction.TYPE.BUY);
-        transaction.setStock(new Stock());
-        transaction.setExerciseDate(dateFormat.parse("2020/01/01"));
 
-        Set<ConstraintViolation<Transaction>> violations = validator.validate(transaction);
-        assertThat(violations.size()).isEqualTo(0);
     }
 
+    @Test
+    public void givenTransactionPOJO_whenDataInvalid_shouldNotValidate() {
+
+    }
+
+    @Test
+    public void givenTransactionPOJO_whenPriceHasLongFraction_shouldNotValidate() {
+
+    }
+
+    @Test
+    public void givenTransactionPOJO_whenPriceHasLongIntegral_shouldNotValidate() {
+
+    }
+
+    @Test
+    public void givenTransactionPOJO_whenPriceIsZero_shouldNotValidate() {
+
+    }
+
+    @Test
+    public void givenTransactionPOJO_whenPriceIsNegative_shouldNotValidate() {
+
+    }
 }
