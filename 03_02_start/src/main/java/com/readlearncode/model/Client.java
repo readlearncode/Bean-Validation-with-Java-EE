92400e62c928c4e0953c06ef726eaf16fab7421b
Alex Theedom <alex.theedom@gmail.com>
2017-11-27 19:37:49 -0800

    add code for module 03_02_start

---
+++ 03_02_start/src/main/java/com/readlearncode/model/Client.java
@@ -0,0 +1,120 @@
+package com.readlearncode.model;
+
+import javax.faces.annotation.FacesConfig;
+import javax.inject.Named;
+import java.util.Date;
+import java.util.Objects;
+
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+
+@Named
+@FacesConfig(version = JSF_2_3)
+public class Client {
+
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String confirmEmail;
+
+    private Date dob; // Date Of Birth
+
+    private Boolean acceptTOS; // Have the Terms Of Service been accepted
+
+    private String creditCardNumber;
+
+    public Client() {
+    }
+
+    public Client(Integer id, String name, Date dob, String email, String confirmEmail, Boolean acceptTOS, String creditCardNumber) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
+        this.confirmEmail = confirmEmail;
+        this.acceptTOS = acceptTOS;
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getConfirmEmail() {
+        return confirmEmail;
+    }
+
+    public void setConfirmEmail(String confirmEmail) {
+        this.confirmEmail = confirmEmail;
+    }
+
+    public Boolean getAcceptTOS() {
+        return acceptTOS;
+    }
+
+    public void setAcceptTOS(Boolean acceptTOS) {
+        this.acceptTOS = acceptTOS;
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + id + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) &&
+                Objects.equals(name, client.name) &&
+                Objects.equals(email, client.email) &&
+                Objects.equals(confirmEmail, client.confirmEmail) &&
+                Objects.equals(dob, client.dob) &&
+                Objects.equals(acceptTOS, client.acceptTOS) &&
+                Objects.equals(creditCardNumber, client.creditCardNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, confirmEmail, dob, acceptTOS, creditCardNumber);
+    }
+}
