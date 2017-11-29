fb16b0c2e7137035959b925ccfd8341fd8f4ca16
Alex Theedom <alex.theedom@gmail.com>
2017-11-29 10:21:26 -0800

    add modules 04_02 start and end states

---
+++ 04_02_start/src/test/java/com/readlearncode/model/ClientTest.java
@@ -0,0 +1,96 @@
+package com.readlearncode.model;
+
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Set;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class ClientTest {
+
+    private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+    private DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+    @Test
+    public void givenClientPOJO_whenDataValid_shouldValidate() throws ParseException {
+        Client client = new Client(1, "John Smith", dateFormat.parse("1978/12/2"), "alex@mydomin.something", true, "5105105105105100");
+        Set<ConstraintViolation<Client>> violations = validator.validate(client);
+        assertThat(violations.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void givenClientPOJO_whenDataInvalid_shouldNotValidate() throws ParseException {
+        Client client = new Client(null, null, null, null, null, "5105105105105100");
+        Set<ConstraintViolation<Client>> violations = validator.validate(client);
+        assertThat(violations.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void givenClientPOJO_whenEmailDataInvalid_shouldValidate() {
+        Set<ConstraintViolation<Client>> violations = validator.validateValue(Client.class, "email", "invalid_email");
+        assertThat(violations.size()).isEqualTo(1);
+    }
+
+    // Challenge solutions
+    @Test
+    public void givenClientPOJO_whenEmailDataValid_shouldValidate() {
+        Set<ConstraintViolation<Client>> violations = validator.validateValue(Client.class, "email", "email@example.com");
+        assertThat(violations.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void givenClientPOJO_whenDateValid_shouldValidate() throws ParseException {
+        Set<ConstraintViolation<Client>> violations = validator.validateValue(Client.class, "dob", dateFormat.parse("1978/12/2"));
+        assertThat(violations.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void givenClientPOJO_whenDateInvalid_shouldValidate() throws ParseException {
+        Set<ConstraintViolation<Client>> violations = validator.validateValue(Client.class, "dob", dateFormat.parse("2035/12/2"));
+        assertThat(violations.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void givenClientPOJO_whenAssertTrueIsTrue_shouldValidate() throws ParseException {
+        Set<ConstraintViolation<Client>> violations = validator.validateValue(Client.class, "acceptTOS", true);
+        assertThat(violations.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void givenClientPOJO_whenAssertTrueIsFalse_shouldValidate() throws ParseException {
+        Set<ConstraintViolation<Client>> violations = validator.validateValue(Client.class, "acceptTOS", false);
+        assertThat(violations.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void givenClientPOJO_whenCreditCardNumberInvalid_shouldValidate() throws ParseException {
+
+    }
+
+    @Test
+    public void givenClientPOJO_whenCreditCardNumberIsValid_shouldValidate() throws ParseException {
+
+    }
+
+    @Test
+    public void givenClientPOJO_whenCreditCardNumberValidWithDashes_shouldValidate()  {
+
+    }
+
+    @Test
+    public void givenClientPOJO_whenCreditCardNumberInvalid_shouldNotValidate()  {
+
+    }
+}
